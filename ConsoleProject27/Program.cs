01fcd6029c53ba6d81574ed01e333ccbecd6747c
Section2 <Section2@WISSEN>
2019-07-10 15:17:05 +0300

    c-projeler

---
+++ ConsoleProject27/Program.cs
@@ -0,0 +1,147 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ConsoleProject27
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+
+
+//            while (true)
+//            {
+//                int sayi;
+//                int sonuc=1;
+
+//                try
+//                {
+//                    Console.WriteLine("faktöriyel sayısını yazınız");
+//                    sayi = Convert.ToInt32(Console.ReadLine());
+//                }
+//                catch
+//                {
+//                    Console.WriteLine("hatalı değer girdiniz");
+//                    continue;
+//                }
+//                while(true)
+//                {
+
+
+//                    for (int i=1;i<=sayi;i++)
+
+//                    {
+//                        sonuc=sonuc*i;
+
+//                    }
+//                    Console.WriteLine(sonuc);
+//                    Console.ReadLine();
+
+//                    break;
+//                }
+
+
+//            }
+
+
+
+//        }
+//    }
+//}
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ConsoleProject27
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            while (true)
+//            {
+//                int girilen;
+//                int sonuc = 1;
+//                string okunan = "";
+//                int i;
+
+//                try
+//                {
+//                    Console.WriteLine("faktöriyel sayısını yazınız");
+//                    girilen = Convert.ToInt32(Console.ReadLine());
+//                }
+//                catch
+//                {
+//                    Console.WriteLine("hatalı değer girdiniz");
+//                    continue;
+//                }
+
+//                for (i = 1; i <= girilen; i++)
+//                {
+//                    sonuc = sonuc * i;
+//                    if (i == girilen)
+//                    {
+//                        okunan = okunan + i;
+//                        Console.WriteLine(okunan);
+//                    }
+//                    else
+//                    {
+//                        okunan = okunan + i + " * ";
+//                    }
+//                    if(girilen==0)
+//                     {
+//                     okunan="0 sayısının faktöriyeli 1";
+//                      console.writeline(okunan);
+//                }
+//                Console.WriteLine("sayıların faktöriyeli " + okunan + " = " + sonuc);
+//                Console.ReadLine();
+//            }//while
+//        }
+//    }
+//}
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleProject27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int girilen;
+                int sonuc = 1;
+                
+                int i;
+
+                try
+                {
+                    Console.WriteLine("faktöriyel sayısını yazınız");
+                    girilen = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("hatalı değer girdiniz");
+                    continue;
+                }
+
+                for (i = 1; i <= girilen; i++)
+                {
+                    sonuc = sonuc * i;
+                  
+                    Console.WriteLine(i + "sayısının faktöriyeli= " +sonuc);
+                }
+                Console.ReadLine();
+            }//while
+        }
+    }
+}
---
+++ ConsoleProject29/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleProjectt27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int faktoriyel = 1;
+            string faktoriyelSonucu = " ";
+            int kacaKadar;
+
+            Console.WriteLine("Hangi sayının faktöriyelini hesaplamak istiyorsun?");
+            kacaKadar = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= kacaKadar; i++)
+            {
+                faktoriyel = faktoriyel * i;
+                if (i < kacaKadar)
+                {
+                    faktoriyelSonucu = faktoriyelSonucu + i + "x";
+                }
+                else if (i == kacaKadar)
+                {
+                    faktoriyelSonucu = faktoriyelSonucu + i;
+                }
+                if (i < kacaKadar)
+                {
+                    string tmp = faktoriyelSonucu;
+                    if (faktoriyelSonucu.Length > 0)
+                        faktoriyelSonucu = faktoriyelSonucu.Substring(0, faktoriyelSonucu.Length - 1);
+
+                    Console.WriteLine(faktoriyelSonucu + " = " + faktoriyel);
+                    faktoriyelSonucu = tmp;
+                }
+                else
+                    Console.WriteLine(faktoriyelSonucu + " = " + faktoriyel);
+
+                Console.ReadLine();
+
+
+            }
+        }
+    }
+}
+
+
