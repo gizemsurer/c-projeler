26800cfa960d53fe2c39fb31c048846a0ef0f100
Section2 <Section2@WISSEN>
2019-07-10 09:43:05 +0300

    c-projeler

---
+++ ConsoleProject26/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleProject26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//eleman sayısını kendimiz girdiğimiz fibonacci dizisi oluşturma programı
+            int x = 0;
+            int y = 1;
+            int sayi;
+            int z;
+            Console.WriteLine("kaç adet fibonacci dizisi toplanacak");
+            sayi = Convert.ToInt32(Console.ReadLine());
+            Console.Write(x + " " + y+ " ");
+           
+            for(int i =3;i<=sayi; i++)
+            {
+                
+               
+                z = x + y;
+                Console.Write( z+" ");
+                x = y; 
+                y = z;
+            }
+            
+            Console.ReadLine();
+
+        }
+    }
+}
