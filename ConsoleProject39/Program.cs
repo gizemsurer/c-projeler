0a03c2211c560728f56063f93fe32d0980221ace
Section1 <Section1@WISSEN>
2019-07-17 15:15:00 +0300

    c-projeler

---
+++ ConsoleProject39/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleProject39
+{
+    class Program
+    {
+        static void Main(string[] args)
+            //açılan parantez kadar kapanan parantezin olduğu ve elemanlarını ters çevirip
+            //parantezleri kaldıran uygulama
+        {
+
+        }
+    }
+}
