3e23ad7accabd5195fb074bf7657ffe28ab24b59
Section1 <Section1@WISSEN>
2019-07-17 15:06:13 +0300

    c-projeler

---
+++ ConsoleProject38/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleProject38
+{
+    class Program
+    {//kullanıcı alacağı para üstünü girsin
+        //para üstü veren makinada sadece 2,5,10'luk banknotlar bulunmaktadır
+        //kullanıcıya en az adet banknot ile paraüstü veren uygulamayı yazınız 
+        //eğer kullanıcı para üstü olarak direkt 1 veya 3 yazarsa sistem uyarı mesajı versin
+        static void Main(string[] args)
+        {
+            int onlukAdedi;
+            int beslikAdedi;
+            int ikilikAdedi;
+            int kalan;
+            int paraUstu;
+            Console.WriteLine("lütfen para üstü giriniz.");
+            paraUstu=Convert.ToInt32(Console.ReadLine());
+
+            onlukAdedi =paraUstu / 10;
+            paraUstu = paraUstu % 10;
+
+            beslikAdedi = paraUstu / 5;
+            paraUstu = paraUstu % 5;
+
+            ikilikAdedi = paraUstu / 2;
+            paraUstu = paraUstu % 2;
+
+            kalan = paraUstu;
+            
+            
+            if(kalan>0)
+
+            {
+                int ekikilikAdedi;
+                if (beslikAdedi>0)
+                {
+                    
+                    beslikAdedi--;
+                    ekikilikAdedi=(5 + kalan) / 2;
+                    ikilikAdedi = ikilikAdedi + ekikilikAdedi;
+                    kalan = 0;
+                }
+                else
+                {
+                    if(onlukAdedi>0)
+                    {
+                        onlukAdedi--;
+                        beslikAdedi++;
+                        ekikilikAdedi = (5 + kalan) / 2;
+                        ikilikAdedi = ikilikAdedi + ekikilikAdedi;
+                        kalan = 0;
+                    }
+                    else
+                    {
+                        Console.WriteLine("lütfen 1 veya 3 dışında bir para üstü girin.");
+                        Console.ReadLine();
+                        return;
+                    }
+                    
+                }
+            }
+            Console.WriteLine("onluk adet sayısı=" + onlukAdedi);
+            Console.WriteLine("beşlik adet sayısı=" + beslikAdedi);
+            Console.WriteLine("ikilik adet sayısı=" + ikilikAdedi);
+            Console.WriteLine("kalan = " + kalan);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ConsoleProjectBanknot/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleKalanPara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double Uruna1adedi;
+            Double Uruna2adedi;
+            Double toplam = 0;
+            double paraUstu;
+            Double girilenTutar = 0;
+            double kalan = 0;
+            int bolum;
+            Console.WriteLine("ürüna1 adedini giriniz");
+            Uruna1adedi = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("ürüna2 adedini giriniz");
+            Uruna2adedi = Convert.ToDouble(Console.ReadLine());
+            toplam = (Uruna1adedi * 0.5) + (Uruna2adedi * 2);
+            Console.WriteLine("ödeyeceğiniz tutar: " + toplam);
+            Console.WriteLine("toplamdan büyük bir tutar giriniz");
+            girilenTutar = Convert.ToDouble(Console.ReadLine());
+            paraUstu = girilenTutar - toplam;
+            Console.WriteLine("para üstünüz:" + paraUstu);
+            bolum = (int)paraUstu / 10;
+            kalan = paraUstu - (bolum * 10);
+            Console.WriteLine("10'luk: " + bolum);
+            bolum = (int)kalan / 5;
+            kalan = kalan - (bolum * 5);
+            Console.WriteLine("5'lik: " + bolum);
+            bolum = (int)kalan / 2;
+            kalan = kalan - (bolum * 2);
+            Console.WriteLine("2'lik: " + bolum);
+            bolum = (int)kalan / 1;
+            Console.WriteLine("kalan miktar " + bolum);
+            if (kalan % 2 == 1)
+            {
+                Console.WriteLine("para üstü verilmeyecetir");
+
+            }
+            Console.ReadLine();
+
+
+
+        }
+
+
+    }
+}
+
