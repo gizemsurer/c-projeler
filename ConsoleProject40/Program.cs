00c5c07a0d0d92b308f07725a78176501c7fc805
Section1 <Section1@WISSEN>
2019-07-19 08:45:46 +0300

    c-projeler

---
+++ ConsoleProject40/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleProject40
+{
+    class Program
+    {//Fibonacci numbers oluşturma projemizi FibonacciNumbers classı oluşturarark çözelim
+     //yani Fibonacci Numbers classı ekranda girilen sayıya kadar olan FibonacciNumber'ları
+     //bizim içinde foreach ile gezebileceğimiz bir collection'a(koleksiyona)çevirsin
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("lütfen ulaşmak istediğiniz fibonacci sayısıı giriniz");
+            int hedefFibonacciSayisi = Convert.ToInt32(Console.ReadLine());
+
+            FibonacciNumbers f = new FibonacciNumbers(hedefFibonacciSayisi);
+            foreach (int n in f)
+            {
+                Console.Write(n);
+            }
+            Console.ReadKey();
+            
+            
+        }
+    }
+}
+class FibonacciNumbers : IEnumerable
+{
+    private FibonacciEnumaretor iter;
+    public FibonacciNumbers(int max)
+    {
+        iter = new FibonacciEnumaretor(max);
+    }
+    public IEnumerator GetEnumerator()
+    {
+        return iter;
+    }
+
+}
+class FibonacciEnumaretor:IEnumerator
+{
+    private int currentNumber, nextNumber, maxNumber;
+    public FibonacciEnumaretor(int max)
+    {
+        maxNumber = max;
+        Reset();
+    }
+    public object Current
+    {
+        get
+        {
+            return currentNumber;
+        }
+    }
+    public bool MoveNext()
+    {
+        int newNextNumber = currentNumber + nextNumber;
+        currentNumber = nextNumber;
+        nextNumber = newNextNumber;
+        return currentNumber <= maxNumber;
+    }
+    public void Reset()
+    {
+        currentNumber = 0;
+        nextNumber = 1;
+    }
+}
---
+++ ConsoleProject42/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleProject42
+{
+    class Program
+    {
+       public enum DersKodlari
+        {
+            MSWORD = 100,
+            MSEXCEL = 111,
+            MSPOWERPOINT = 222,
+            MSOUTLOOK = 333
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Lütfen W, E, P, O harflerinden birini giriniz.");
+                char c = Convert.ToChar(Console.ReadLine().ToUpper());
+                int dersSaati = 0;
+                int s = HarfeGoreDersBelirle(c);
+                if (s == (int)DersKodlari.MSWORD)//s==100 
+                    dersSaati = 10;
+                else if (s == 111)//s==(int)DersKodlari.MSEXCEL bu şekilde kullanmak sayı kullnamaktan daha yararlı
+                    dersSaati = 30;
+                else if (s == 222)//s==(int)DersKodlari.POWERPOINT
+                    dersSaati = 5;
+                else if (s == 333)//s==(int)DersKodlari.MSOUTLOOK
+                    dersSaati = 15;
+                else
+                {
+                    dersSaati = 0;
+                    continue;
+                }
+                Console.WriteLine(s + " dersinin haftalık ders saati = " + dersSaati);
+                Console.ReadLine();
+            }
+        }
+        public static int HarfeGoreDersBelirle(char dersHarfi)
+        {
+            string dersAdi = "";
+            int dersKodu = 0;
+            if (dersHarfi == 'W')
+            {
+                dersAdi = "MSWORD";
+                dersKodu = (int)DersKodlari.MSWORD;
+            }
+            else if (dersHarfi == 'E')
+            {
+                dersAdi = "MSEXCEL";
+                dersKodu = (int)DersKodlari.MSEXCEL;
+            }
+            else if (dersHarfi == 'P')
+            {
+                dersAdi = "MSPOWERPOINT";
+                dersKodu = (int)DersKodlari.MSPOWERPOINT;
+            }
+            else if (dersHarfi == 'O')
+            {
+                dersAdi = "MSOUTLOOK";
+                dersKodu = (int)DersKodlari.MSOUTLOOK;
+            }
+            else
+                Console.WriteLine("Geçerli bir harf girmediniz!");
+            if (dersAdi != "")
+                Console.WriteLine(dersAdi);
+            return dersKodu;
+        }
+    }
+}
